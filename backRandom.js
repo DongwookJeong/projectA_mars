96f72b34fc1525cf4a5f5a85415a0e450a8f3412
정동욱 <nolhoon211@gmail.com>
2022-11-09 14:32:19 +0900

    오전 중간 과정, 아직 동일 작업중

---
+++ backRandom.js
@@ -0,0 +1,30 @@
+const imgs = [
+  "./images/ghibli.jpg",
+  "./images/ghibli2.jpg",
+  "./images/ghibli3.jpg",
+];
+const babyImgs = [
+  "./images/page.png",
+  "./images/page2.png",
+  "./images/page3.png",
+]
+/*
+! 자 필요한 것들
+? 랜덤 함수
+? if() ghibli.jpg일때 page.png
+? 공통점 뒤 파일확장자, .n
+? module 모아서 슝~
+*/
+
+// const selectImage = imgs[Math.floor(Math.random() * imgs.length)];
+// console.log(selectImage);
+// console.log(imgs);
+// console.log(selectImage);
+// // if(selectImage.valueOf ===)
+// function select(value){
+//   if(selectImage.valueOf === "./images/ghibli.jpg"){
+//     for(let i = 0; i <= "./images"; i++){
+
+//     }
+//   }
+// }
--- ghibli.js
+++ ghibli.js
@@ -1,34 +1,65 @@
-// import apiJs from "./ghibliAPI.js";
+const root = document.getElementById("root");
+const backgroundImg = document.createElement("div");
+const header = document.createElement("header")
+const home = document.createElement("div")
+root.appendChild(header)
+root.appendChild(backgroundImg);
+header.appendChild(home)
 
+function styling(element, styleProps) {
+  for (const miya in styleProps) {
+    element.style[miya] = styleProps[miya];
+  }
+}
+
+const rootStyle = {
+  width: "100vw",
+  height: "100vh",
+  margin: 0,
+  display: "flex",
+  flexDirection: "column",
+  
+};
+const headerStyle = {
+  position: "absolute",
+  width: "inherit",
+  height: "5vh"
+}
+// ? 고정 이미지
+const homeStyle = {
+  width: "45px",
+  height: "inherit",
+  backgroundImage: `URL("./images/homebu.png")`,
+  backgroundSize: "cover",
+}
+// ! 랜덤 이미지
+const backStyle = {
+  width: "inherit",
+  height: "inherit",
+  backgroundImage: `URL("./images/ghibli.jpg")`,
+  backgroundSize: "cover",
+  margin: 0
+};
+styling(root, rootStyle);
+styling(header, headerStyle)
+styling(backgroundImg, backStyle);
+styling(home, homeStyle)
+
+/*
+? 11월 8일자 api 시도, 이미지 넣기
+import apiJs from "./ghibliAPI.js";
 const makeItTag = (parent, tagName, html) => {
   const tag = document.createElement(tagName);
   tag.innerHTML = html;
   parent.appendChild(tag);
 };
-// function styling(element, styleProps, textContent = "") {
-//   for (const miya in styleProps) {
-//     element.style[miya] = styleProps[miya];
-//   }
-//   element.textContent = textContent;
-// }
-const root = document.getElementById("root");
-makeItTag(
-  root,
-  "header",
-  `<div><img style="width : 70px; height : 70px; src="./images/home.png"/></div>`
-);
+makeItTag(root, "header", `<div><img src="./images/home.png"/></div>`);
 
-// const rootStyle = {
-//   width: "100vw",
-//   height: "100vh",
-// };
-// styling(root, rootStyle);
 
-// const div = document.createElement("div");
-// root.appendChild(div);
 
-// export default apiJs(arr, imgArr).then(() => {
-//   for (let i = 0; i < arr.length; i++) {
-//     div.innerHTML += `<div><div>${apiJs[i]}</div></div>`;
-//   }
-// });
+export default apiJs(arr, imgArr).then(() => {
+  for (let i = 0; i < arr.length; i++) {
+    div.innerHTML += `<div><div>${apiJs[i]}</div></div>`;
+  }
+});
+*/
