5152c56ae123befa7d070c80384dbe10e1633c7c
정동욱 <nolhoon211@gmail.com>
2022-11-10 16:33:56 +0900

    랜덤 확인 파일

--- backRandom.js
+++ backRandom.js
@@ -8,14 +8,44 @@ const babyImgs = [
   "./images/page2.png",
   "./images/page3.png",
 ];
-export default function random() {
-  let selectImage = imgs[Math.floor(Math.random() * imgs.length)];
-  console.log(selectImage);
-  return selectImage;
+// export default function random() {
+//   let selectImage = imgs[Math.floor(Math.random() * imgs.length)];
+//   console.log(selectImage);
+
+//   return selectImage;
+// }
+let imgThing = imgs[Math.floor(Math.random() * 3)];
+function one(babyImg){
+  // let img = imgThing
+  // let babyImgs
+  // console.log(imgThing)
+  if(imgThing === './images/ghibli.jpg'){
+    babyImg = "./images/page.png"
+    // return (imgThing,babyImg)
+  }else if(imgThing ==='./images/ghibli2.jpg'){
+    babyImg = "./images/page2.png"
+    // return (imgThing, babyImg)
+  }else if(imgThing ==='./images/ghibli3.jpg'){
+    babyImg = "./images/page3.png"
+    
+  }
+  return (babyImg)
 }
+console.log(imgThing)
+console.log(one())
+// console.log(img)
+// let two = one()
 
+/*
 random();
+function abc(imgs){
+  let wow = imgs[Math.floor(Math.random() * imgs.length)];
 
+  imgs(wow)
+}
+abc(wow)=>{
+}
+*/
 /*
 ! 자 필요한 것들
 ? 랜덤
